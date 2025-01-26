83fac84bcd4764a0781b53f72f9db88bb2e7d3a1
KrishnaCodez <urvishpvt@gmail.com>
2025-01-26 21:24:20 +0530

    server & db connected & clerk setup

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <ClerkProvider>{children}</ClerkProvider>;
+}
