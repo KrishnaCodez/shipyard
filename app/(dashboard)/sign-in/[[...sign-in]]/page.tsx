b2c5d404aabd2c2ada6512a2fcb4091ff59dc423
KrishnaCodez <urvishpvt@gmail.com>
2025-01-27 11:11:50 +0530

    add sign-up and sign-in pages with Clerk integration; implement utility functions and update Tailwind configuration

---
+++ app/(dashboard)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import * as Clerk from "@clerk/elements/common";
+import { SignIn, SignInButton } from "@clerk/nextjs";
+
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-[100dvh] relative items-center justify-center bg-gray-100 px-4 dark:bg-white">
+      <div className="absolute inset-0 z-0 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+      </div>
+      <div className="absolute top-0 z-[-1] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+      <div className="mx-auto z-10 text-gray-700 w-full max-w-[500px]">
+        <SignIn signUpUrl="/sign-up" />
+      </div>
+    </div>
+  );
+}
+
+
