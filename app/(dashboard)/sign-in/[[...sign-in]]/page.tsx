147c6fe04a34fb2b410efe94551638adcfac2465
KrishnaCodez <urvishpvt@gmail.com>
2025-02-02 15:06:47 +0530

    eslient disable

--- app/(dashboard)/sign-in/[[...sign-in]]/page.tsx
+++ app/(dashboard)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import * as Clerk from "@clerk/elements/common";
 import * as SignIn from "@clerk/elements/sign-in";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
   Card,
